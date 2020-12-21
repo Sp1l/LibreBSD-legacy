86c00e68ae7c0c7ba9d33016ac6c82c00ffc13ef
Bernard Spil <brnrd@FreeBSD.org>
2020-12-21 11:29:10 +0000

    OpenNTPd: Update sources with ports' patches

--- contrib/openntpd/src/ntp_dns.c
+++ contrib/openntpd/src/ntp_dns.c
@@ -33,7 +33,7 @@
 #include "ntpd.h"
 
 volatile sig_atomic_t	 quit_dns = 0;
-struct imsgbuf		*ibuf_dns;
+static struct imsgbuf	*ibuf_dns;
 
 void	sighdlr_dns(int);
 int	dns_dispatch_imsg(void);
--- contrib/openntpd/src/parse.y
+++ contrib/openntpd/src/parse.y
@@ -57,7 +57,6 @@ int		 lgetc(int);
 int		 lungetc(int);
 int		 findeol(void);
 
-struct ntpd_conf		*conf;
 struct sockaddr_in		 query_addr4;
 struct sockaddr_in6		 query_addr6;
 
